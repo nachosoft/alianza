bfb0b85eabd52379729f83e063aad8b21f755fba
vipomx <vipopmx@gmail.com>
2016-06-24 21:21:25 -0600

    I don't what I does

---
+++ sites/all/modules/custom/unit_paying/theme/unit_paying_poliza.tpl.php
@@ -0,0 +1,16 @@
+<div class="page">
+  <div class="inner_page">
+    <div class="header">
+      <h2 class="title">ALIANZA DE AUTO TRANSPORTES  DE TEPIC</h2>
+      <p class="lugar">OFICINAS GENERALES: RIO GRIJALVA No. 114 COL. LOS FRESNOS PTE.</p>
+      <div class="info">
+        <p class="fax">FAX.210-55-14</p>
+        <p class="tels">TELS. 210-55-15 Y 210-55-16</p>
+        <p class="place">TEPIC, NAYARIT</p>
+      </div>
+    </div>
+    <div class="content">
+      <div class="data"></div>
+    </div>
+  </div>
+</div>
